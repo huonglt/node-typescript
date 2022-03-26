92e89c528230a9d84629480954e988512f4c904e
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-26 22:09:32 +1300

    repo setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+};
